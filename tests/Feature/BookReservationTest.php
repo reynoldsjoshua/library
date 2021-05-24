e6c0a424bdb3fb313015ec9706ce6d42916adeeb
Josh Reynolds <82967109+reynoldsjoshua@users.noreply.github.com>
2021-05-24 14:31:41 -0500

    adds basic test for Book

---
+++ tests/Feature/BookReservationTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    /** @test */
+    public function a_book_can_be_added_to_the_library()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $response = $this->post('/books', [
+            'title' => 'Cool Book Title',
+            'author' => 'Josh',
+        ]);
+        
+        $response->assertOk();
+        $this->assertCount(1, Book::all());
+    }
+
+    /** @test */
+    public function a_title_is_required()
+    {
+
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'Josh',
+        ]);
+        
+        $response->assertSessionHasErrors('title');
+
+    }
+
+    /** @test */
+    public function a_author_is_required()
+    {
+
+        $response = $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => '',
+        ]);
+        
+        $response->assertSessionHasErrors('author');
+
+    }
+
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+
+
+        $this->post('/books', [
+            'title' => 'Cool Title',
+            'author' => 'Josh',
+        ]);
+
+        $book = Book::first();
+        
+        $response = $this->patch('/books/' . $book->id, [
+            'title' => 'New Title',
+            'author' => 'New Author',
+        ]);
+
+        $this->assertEquals('New Title', Book::first()->title);
+        $this->assertEquals('New Author', Book::first()->author);
+
+    }
+}
