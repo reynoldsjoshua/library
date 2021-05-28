33a9285cec539da7c4866ea0d2fddd562064af0f
Josh Reynolds <82967109+reynoldsjoshua@users.noreply.github.com>
2021-05-28 09:06:19 -0500

    More Author testing and functions

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,13 +6,15 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Author extends Model
 {
     protected $guarded = [];
 
     protected $dates = ['dob'];
 
-    public function setDobAttribute($dob){
+    public function setDobAttribute($dob)
+    {
         $this->attributes['dob'] = Carbon::parse($dob);
     }
 }
