885e822c8d45a726f0c929a01bcb70a12157d348
Josh Reynolds <82967109+reynoldsjoshua@users.noreply.github.com>
2021-05-28 09:05:26 -0500

    More Book testing and functions

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,16 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     public function path()
     {
         return '/books/' . $this->id;
+    }
+
+    public function setAuthorIdAttribute($author)
+    {
+        $this->attributes['author_id'] = (Author::firstOrCreate([
+            'name' => $author,
+        ]))->id;
     }
 }
